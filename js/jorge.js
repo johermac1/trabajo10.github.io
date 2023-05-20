220400d06105620a817584b83e7acee45319a117
jorge Maciel <130870099+johermac1@users.noreply.github.com>
2023-05-20 15:33:00 -0300

    Add files via upload

---
+++ js/jorge.js
@@ -0,0 +1,12 @@
+let nombre = "jorge";
+let apellido = "maciel";
+
+function nombreyapellido(){
+
+
+  let nombreapellido = (nombre + " " + apellido);
+  console.log(nombreapellido);
+
+}
+
+nombreyapellido();
---
+++ js/jorge1.js
@@ -0,0 +1,15 @@
+// Crear dos variables numéricas
+let num1 = 5;
+let num2 = 10;
+
+// Crear una tercera variable llamada "suma" que suma las primeras dos
+let suma = num1 + num2;
+
+// Imprimir el valor de la variable "suma" en la consola
+console.log(suma);
+
+// Crear otra variable numérica y asignarle la suma entre "suma" y un número
+let num3 = suma + 7;
+
+// Imprimir el valor de la nueva variable en la consola
+console.log(num3);
